b907f96c8773fab46d2f52d6ed2e26aaf1028d66
JHobbs96 <43074826+JHobbs96@users.noreply.github.com>
2021-10-04 15:53:14 -0700

    Delete HW2 directory

--- HW2/src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-import vault.PasswordVault;
-
-public class Main {
-
-    public static void main(String[] args) {
-        PasswordVault vault = new PasswordVault();
-    }
-}
--- HW2/src/exceptions/InvalidSiteException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class InvalidSiteException extends Exception {
-    private static final long serialVersionUID = 1L;
-    public InvalidSiteException(){
-        super("The site name is invalid; enter 6-12 lower-case letters.\n");
-    }
-}
