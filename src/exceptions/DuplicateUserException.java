7e9fa533122b18ed7e0a509c85c26e3f20327f29
JHobbs96 <43074826+JHobbs96@users.noreply.github.com>
2021-10-04 19:39:39 -0700

    Delete src directory

--- src/exceptions/DuplicateUserException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class DuplicateUserException extends Exception {
-
-    private static final long serialVersionUID = 1L;
-    public DuplicateUserException(){
-        super("The username already exists.\n");
-    }
-}
--- src/exceptions/PasswordMismatchException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class PasswordMismatchException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public PasswordMismatchException(){
-        super("Attempted to login with incorrect credentials.\n");
-    }
-}
